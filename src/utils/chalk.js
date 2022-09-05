f7d314dfe5f3cde2f29c7b5c03ab8befbc0c0ce0
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-09-05 19:49:57 -0300

    feat: add chalk colors

---
+++ src/utils/chalk.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+const cliName = chalk.cyanBright;
+const tableHeaders = chalk.yellowBright;
+const errorMessage = chalk.red;
+
+export { cliName, tableHeaders, errorMessage };
