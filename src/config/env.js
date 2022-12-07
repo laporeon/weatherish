31eb02cda6a0319b4347418b96fabd725ff7ca50
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-12-07 19:13:36 -0300

    refactor: change used API and create new logic

---
+++ src/config/env.js
@@ -0,0 +1,3 @@
+export const env = {
+  apiKey: process.env.API_KEY ?? '',
+};
--- src/utils/chalk.js
+++ src/utils/chalk.js
@@ -1,7 +1,6 @@
 import chalk from 'chalk';
 
 const cliName = chalk.cyanBright;
-const tableHeaders = chalk.yellowBright;
 const errorMessage = chalk.red;
 
-export { cliName, tableHeaders, errorMessage };
+export { cliName, errorMessage };
