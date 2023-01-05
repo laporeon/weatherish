4dd021fd18250fcf235d34180c9d1e3e421e343a
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-01-05 18:47:01 -0300

    refactor: create enum for measurement units

---
+++ src/utils/units.js
@@ -0,0 +1,12 @@
+export const Units = {
+  metric: {
+    localTimeFormat: 'pt-BR',
+    temperatureUnit: '°C',
+    windSpeedUnit: 'km/h',
+  },
+  imperial: {
+    localTimeFormat: 'en-US',
+    temperatureUnit: '°F',
+    windSpeedUnit: 'mph',
+  },
+};
