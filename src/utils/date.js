08c1cc32b188aa3614c262f85c1969971664b748
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-01-11 12:51:15 -0300

    style(eslint): add eslint

--- src/utils/date.js
+++ src/utils/date.js
@@ -3,6 +3,6 @@ export function formatDate(date, localTimeFormat) {
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
-    minute: '2-digit'
-  }).format(date)
+    minute: '2-digit',
+  }).format(date);
 }
