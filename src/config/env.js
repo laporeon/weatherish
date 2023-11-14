ec897aa459b5df6289a7d7b1d5b6a7c44271e461
laporeon <34722707+laporeon@users.noreply.github.com>
2023-11-14 17:33:59 -0300

    refactor: change baseURL

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,3 @@
 export const env = {
-  api_key: '3a1644de217813e1534790891834d5e3',
-  api_base_url: 'https://api.openweathermap.org/',
-  api_path: 'data/2.5/weather?q=',
+  baseApiURL: 'https://weatherish-bff.onrender.com/',
 };
