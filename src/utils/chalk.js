6f5a35c4f410c9d8edfb6ce730079f32aa009429
daspeon <175569928+daspeon@users.noreply.github.com>
2024-11-19 18:04:45 -0300

    refactor: delete modified files

--- src/utils/chalk.js
+++
@@ -1,6 +0,0 @@
-import chalk from 'chalk';
-
-const errorMessage = chalk.red;
-const title = chalk.blue.underline.bold;
-
-export { errorMessage, title };
--- src/utils/date.js
+++
@@ -1,8 +0,0 @@
-export function formatDate(date, localTimeFormat) {
-  return new Intl.DateTimeFormat(`${localTimeFormat}`, {
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-  }).format(date);
-}
