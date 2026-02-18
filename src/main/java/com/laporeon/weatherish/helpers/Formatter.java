c036a7fe3ce808352ba9826be205288f48a18d0d
laporeon <175569928+laporeon@users.noreply.github.com>
2026-02-18 16:39:33 -0300

    feat: create method to format output response

--- src/main/java/com/laporeon/weatherish/helpers/Formatter.java
+++ src/main/java/com/laporeon/weatherish/helpers/Formatter.java
@@ -1,18 +1,43 @@
 package com.laporeon.weatherish.helpers;
 
 import com.laporeon.weatherish.enums.Unit;
+import com.laporeon.weatherish.models.Weather;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Formatter {
 
-    public String formatWindSpeed(String units, double speed, String speedUnit) {
-        if (units.equals(Unit.IMPERIAL.name())) {
-            return String.format("%.0f %s", speed, speedUnit);
+    public static String formatWindSpeed(double speed, String unit) {
+        Unit unitEnum = Unit.fromString(unit);
+
+        if (unitEnum == Unit.IMPERIAL) {
+            return String.format("%.0f %s", speed, unitEnum.getWindSpeedUnit());
         }
 
-        return String.format("%.0f %s", speed * 3.6, speedUnit);
+        return String.format("%.0f %s", speed * 3.6, unitEnum.getWindSpeedUnit());
     }
 
-    public String formatTemperature(double temperature,String tempUnit) {
-        return String.format("%.0f %s", temperature, tempUnit);
+    public static String formatTemperature(double temperature, String unit) {
+        return String.format("%.0f%s", temperature, Unit.fromString(unit).getTemperatureUnit());
+    }
+
+    public static String formatOutput(Weather data, String unit) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy, HH:mm");
+
+        String name = data.name;
+        String temperature = Formatter.formatTemperature(data.main.temp, unit);
+        String feelsLike = Formatter.formatTemperature(data.main.feelsLike, unit);
+        String wind = Formatter.formatWindSpeed(data.wind.speed, unit);
+        String humidity = String.format("%.0f %%", data.main.humidity);
+
+        return """
+                    📌 City: %s
+                    🕓 Local Time: %s
+                    ⛅ Temperature: %s
+                    🔥 Feels like: %s
+                    💨 Wind: %s
+                    💦 Humidity: %s
+                """.formatted(name, LocalDateTime.now().format(dateTimeFormatter), temperature, feelsLike, wind, humidity);
     }
 }
