49bfc2d0b994dd0b82078036b3bd9ad83c0384f7
laporeon <175569928+laporeon@users.noreply.github.com>
2026-02-18 16:36:00 -0300

    refactor: change response type and parse body with Gson

--- src/main/java/com/laporeon/weatherish/client/WeatherClient.java
+++ src/main/java/com/laporeon/weatherish/client/WeatherClient.java
@@ -1,5 +1,7 @@
 package com.laporeon.weatherish.client;
 
+import com.google.gson.Gson;
+import com.laporeon.weatherish.models.Weather;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import java.net.URI;
@@ -12,7 +14,7 @@ public class WeatherClient {
     private static final String BASE_URL = "https://api.openweathermap.org/data/2.5";
     private static final String API_KEY = Dotenv.load().get("API_KEY");
 
-    public String getData(String city, String unit) throws Exception {
+    public Weather getData(String city, String unit) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                                          .uri(URI.create(BASE_URL + "/weather?q=" + city + "&appid=" + API_KEY +
@@ -20,7 +22,9 @@ public class WeatherClient {
                                          .GET()
                                          .build();
 
-        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+
+        return new Gson().fromJson(body, Weather.class);
     }
 
 }
