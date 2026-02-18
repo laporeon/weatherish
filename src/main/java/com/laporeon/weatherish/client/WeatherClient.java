db23118465b8a4c37e490612836cddd2ddc245de
laporeon <175569928+laporeon@users.noreply.github.com>
2026-02-18 15:14:38 -0300

    feat: create httpclient with BASE_URL and get weather data method

---
+++ src/main/java/com/laporeon/weatherish/client/WeatherClient.java
@@ -0,0 +1,26 @@
+package com.laporeon.weatherish.client;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WeatherClient {
+
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5";
+    private static final String API_KEY = Dotenv.load().get("API_KEY");
+
+    public String getData(String city, String unit) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                                         .uri(URI.create(BASE_URL + "/weather?q=" + city + "&appid=" + API_KEY +
+                                                 "&units=" + unit))
+                                         .GET()
+                                         .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+    }
+
+}
