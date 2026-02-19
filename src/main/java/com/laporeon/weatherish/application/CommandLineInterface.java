3d537636982511c994ea7bd2dc7cf50c57beae48
laporeon <175569928+laporeon@users.noreply.github.com>
2026-02-19 15:54:52 -0300

    feat: create application package with core funcionality of CLI ui

---
+++ src/main/java/com/laporeon/weatherish/application/CommandLineInterface.java
@@ -0,0 +1,79 @@
+package com.laporeon.weatherish.application;
+
+import com.laporeon.weatherish.client.WeatherClient;
+import com.laporeon.weatherish.enums.Color;
+import com.laporeon.weatherish.enums.Unit;
+import com.laporeon.weatherish.helpers.Formatter;
+import com.laporeon.weatherish.models.Weather;
+
+import java.util.Scanner;
+
+public class CommandLineInterface {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void start() throws Exception {
+        displayLogo();
+
+        String city = promptCity();
+        String unit = promptUnit();
+
+        scanner.close();
+
+        Weather data = new WeatherClient().getData(city, unit);
+        String formattedResponse = Formatter.formatOutput(data, unit);
+
+        System.out.printf("\n%s\n", formattedResponse);
+    }
+
+    private String promptCity() {
+        System.out.printf("%sEnter city name: %s", Color.BOLD, Color.RESET);
+        String city = scanner.nextLine().trim();
+
+        while (city.isEmpty()) {
+            System.out.printf("%s%sCity name cannot be empty. Try again...%s\n", Color.ITALIC, Color.RED, Color.RESET);
+            System.out.printf("%sEnter city name: %s", Color.BOLD, Color.RESET);
+            city = scanner.nextLine().trim();
+        }
+
+        return city;
+    }
+
+    private String promptUnit() {
+        System.out.printf("""
+        %sChoose a measurement unit:
+        [1] Metric (°C, km/h)
+        [2] Imperial (°F, mph)%s
+        """, Color.BOLD, Color.RESET);
+        System.out.printf("%sEnter your choice: %s", Color.BOLD, Color.RESET);
+        String input = scanner.nextLine().trim();
+
+        while (!input.equals("1") && !input.equals("2")) {
+            System.out.printf("%s%sInvalid option. Try again...%s\n", Color.ITALIC, Color.RED, Color.RESET);
+            System.out.printf("%sEnter your choice: %s", Color.BOLD, Color.RESET);
+            input = scanner.nextLine().trim();
+        }
+
+        int value = Integer.parseInt(input);
+        return Unit.values()[value - 1].name();
+    }
+
+    private void displayLogo() {
+        clearScreen();
+
+        System.out.printf("%s" + """
+        
+         __        __         _   _               _     _
+         \\ \\      / /__  __ _| |_| |__   ___ _ __(_)___| |__
+          \\ \\ /\\ / / _ \\/ _` | __| '_ \\ / _ \\ '__| / __| '_ \\
+           \\ V  V /  __/ (_| | |_| | | |  __/ |  | \\__ \\ | | |
+            \\_/\\_/ \\___|\\__,_|\\__|_| |_|\\___|_|  |_|___/_| |_|
+        """ + "%s%n", Color.GREEN, Color.RESET);
+    }
+
+    private static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+}
