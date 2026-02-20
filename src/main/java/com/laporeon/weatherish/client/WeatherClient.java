0aa9daeda6f077e6d891883a853d7153929dd80a
laporeon <175569928+laporeon@users.noreply.github.com>
2026-02-20 13:54:17 -0300

    feat: add treatment to error responses on weather client

--- src/main/java/com/laporeon/weatherish/client/WeatherClient.java
+++ src/main/java/com/laporeon/weatherish/client/WeatherClient.java
@@ -1,6 +1,7 @@
 package com.laporeon.weatherish.client;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.laporeon.weatherish.models.Weather;
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -13,18 +14,27 @@ public class WeatherClient {
 
     private static final String BASE_URL = "https://api.openweathermap.org/data/2.5";
     private static final String API_KEY = Dotenv.load().get("API_KEY");
+    private static final String ERROR_MESSAGE = "❌ Error %s: %s. Please check your input and try again.";
 
     public Weather getData(String city, String unit) throws Exception {
+        String url = BASE_URL + "/weather?q=" + city + "&appid=" + API_KEY + "&units=" + unit;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                                         .uri(URI.create(BASE_URL + "/weather?q=" + city + "&appid=" + API_KEY +
-                                                 "&units=" + unit))
+                                         .uri(URI.create(url))
                                          .GET()
                                          .build();
 
-        String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        return new Gson().fromJson(body, Weather.class);
+        if (response.statusCode() != 200) {
+            JsonObject error = new Gson().fromJson(response.body(), JsonObject.class);
+            String status = error.get("cod").getAsString();
+            String message =  error.get("message").getAsString();
+            throw new RuntimeException(ERROR_MESSAGE.formatted(status, message));
+        }
+
+        return new Gson().fromJson(response.body(), Weather.class);
     }
 
 }
