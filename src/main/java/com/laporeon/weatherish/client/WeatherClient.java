d810ae820269791a2d80d88a5ffce539a7e1b370
laporeon <175569928+laporeon@users.noreply.github.com>
2026-02-20 17:18:50 -0300

    refactor: create static variables for httpclient and gson

--- src/main/java/com/laporeon/weatherish/client/WeatherClient.java
+++ src/main/java/com/laporeon/weatherish/client/WeatherClient.java
@@ -15,26 +15,27 @@ public class WeatherClient {
     private static final String BASE_URL = "https://api.openweathermap.org/data/2.5";
     private static final String API_KEY = Dotenv.load().get("API_KEY");
     private static final String ERROR_MESSAGE = "❌ Error %s: %s. Please check your input and try again.";
+    private static final Gson GSON = new Gson();
+    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
 
     public Weather getData(String city, String unit) throws Exception {
         String url = BASE_URL + "/weather?q=" + city + "&appid=" + API_KEY + "&units=" + unit;
 
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                                          .uri(URI.create(url))
                                          .GET()
                                          .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
 
         if (response.statusCode() != 200) {
-            JsonObject error = new Gson().fromJson(response.body(), JsonObject.class);
+            JsonObject error = GSON.fromJson(response.body(), JsonObject.class);
             String status = error.get("cod").getAsString();
             String message =  error.get("message").getAsString();
             throw new RuntimeException(ERROR_MESSAGE.formatted(status, message));
         }
 
-        return new Gson().fromJson(response.body(), Weather.class);
+        return GSON.fromJson(response.body(), Weather.class);
     }
 
 }
