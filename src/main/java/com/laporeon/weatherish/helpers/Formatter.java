66caa8013bd69c5fd1bc6b76a1399f4aff0bcda3
laporeon <175569928+laporeon@users.noreply.github.com>
2026-02-20 13:55:28 -0300

    refactor: change access modifiers of some methods and improve temperature icon

--- src/main/java/com/laporeon/weatherish/helpers/Formatter.java
+++ src/main/java/com/laporeon/weatherish/helpers/Formatter.java
@@ -8,7 +8,26 @@ import java.time.format.DateTimeFormatter;
 
 public class Formatter {
 
-    public static String formatWindSpeed(double speed, String unit) {
+    public String formatOutput(Weather data, String unit) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM, HH:mm");
+
+        String name = data.name;
+        String temperature = formatTemperature(data.main.temp, unit);
+        String feelsLike = formatTemperature(data.main.feelsLike, unit);
+        String wind = formatWindSpeed(data.wind.speed, unit);
+        String humidity = String.format("%.0f%%", data.main.humidity);
+
+        return """
+                    📌 City: %s
+                    🕓 Local Time: %s
+                    🌡 Temperature: %s
+                    🔥 Feels like: %s
+                    💨 Wind: %s
+                    💦 Humidity: %s
+                """.formatted(name, LocalDateTime.now().format(dateTimeFormatter), temperature, feelsLike, wind, humidity);
+    }
+
+    private String formatWindSpeed(double speed, String unit) {
         Unit unitEnum = Unit.fromString(unit);
 
         if (unitEnum == Unit.IMPERIAL) {
@@ -18,26 +37,8 @@ public class Formatter {
         return String.format("%.0f %s", speed * 3.6, unitEnum.getWindSpeedUnit());
     }
 
-    public static String formatTemperature(double temperature, String unit) {
+    private String formatTemperature(double temperature, String unit) {
         return String.format("%.0f%s", temperature, Unit.fromString(unit).getTemperatureUnit());
     }
 
-    public static String formatOutput(Weather data, String unit) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy, HH:mm");
-
-        String name = data.name;
-        String temperature = Formatter.formatTemperature(data.main.temp, unit);
-        String feelsLike = Formatter.formatTemperature(data.main.feelsLike, unit);
-        String wind = Formatter.formatWindSpeed(data.wind.speed, unit);
-        String humidity = String.format("%.0f%%", data.main.humidity);
-
-        return """
-                    📌 City: %s
-                    🕓 Local Time: %s
-                    ⛅ Temperature: %s
-                    🔥 Feels like: %s
-                    💨 Wind: %s
-                    💦 Humidity: %s
-                """.formatted(name, LocalDateTime.now().format(dateTimeFormatter), temperature, feelsLike, wind, humidity);
-    }
 }
